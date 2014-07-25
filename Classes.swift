218e26f939de0f2a50ab38a2ac190f6c9cd8db2d
AdamByrne <AdamByrne@users.noreply.github.com>
2014-07-25 18:29:25 +0100

    Create Classes.swift
    This is my class,
    classes are like a objects
    All variables that use it have methods and properties.

---
+++ Classes.swift
@@ -0,0 +1,13 @@
+class Animal() {
+var name: String = "none"
+var age: Int = 0
+
+func details()-> String {
+println("This animal's name is \(name). It is \(age) years old!")
+}
+}
+
+var Lion = Animal()
+Lion.name = "Fluffy"
+Lion.age = 4
+Lion.details()
