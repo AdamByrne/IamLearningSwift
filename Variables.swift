7114548a5ee1a3da68d617ff76fda7c53af860bd
Adam Byrne <AdamByrne@users.noreply.github.com>
2014-07-29 13:26:04 +0100

    Update Variables.swift

--- Variables.swift
+++ Variables.swift
@@ -1,5 +1,13 @@
+//explicit
 var name: String = "Adam"
 var age: Int = 12
 var human: Bool = true
 var height: Double = 1.4
 var awesome: Bool = true
+
+//implicit
+var name = "Adam"
+var age = 12
+var human = true
+var height = 1.4
+var awesome = true
